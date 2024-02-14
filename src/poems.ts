24349cdbd781a0eb4a2832da12f3ebe1a2383aec
Julian Michael <julianjohnmichael@gmail.com>
2024-02-14 12:19:41 -0800

    Add Shakespeare's Sonnet 54

--- src/poems.ts
+++ src/poems.ts
@@ -163,6 +163,25 @@ of love around you,
 an invisible cloak
 to mind your life.`
     },
+    {
+        title: "Sonnet 54",
+        author: "William Shakespeare",
+        poem:
+            `O, how much more doth beauty beauteous seem
+By that sweet ornament which truth doth give!
+The rose looks fair, but fairer we it deem
+For that sweet odour which doth in it live.
+The canker-blooms have full as deep a dye
+As the perfumèd tincture of the roses,
+Hand on such thorns, and play as wantonly
+When summer's breath their maskèd buds discloses;
+But, for their virtue only is their show,
+They live unwoo'd and unrespected fade,
+Die to themselves. Sweet roses do not so;
+Of their sweet deaths are sweetest odours made.
+ And so of you, beauteous and lovely youth,
+ When that shall fade, my verse distils your truth.`
+    }
 ]
 const presetPoemsMap = presetPoems.reduce((acc, poem) => {
     acc[poem.title] = poem.poem
